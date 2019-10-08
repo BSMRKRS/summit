92db0cdffc72a1c4a1f380eb7208dede4504a0ae
Cooper Hanson <chanson20@bsmschool.org>
2019-10-08 12:06:33 -0500

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,59 @@
+import RoboPiLib as RPL
+import sys,tty,termios,signal,setup,time
+
+race_mode = False
+fd = sys.stdin.fileno()
+old_settings = termios.tcgetattr(fd)
+
+def interrupted(signum, frame):
+    RPL.servoWrite(0,0)
+    RPL.servoWrite(1,1500)
+
+def forward(speed):
+    RPL.servoWrite(2,speed + 1000)
+    time.sleep(0.25)
+    RPL.servoWrite(2,speed)
+
+def backward(speed):
+    RPL.servoWrite(2,speed - 1000)
+    time.sleep(0.25)
+    RPL.servoWrite(2,speed)
+
+def turn(direction):
+    #1000 = left
+    #1500 = straight
+    #2000 = right
+    RPL.servoWrite(1,direction)
+
+
+
+signal.signal(signal.SIGALRM, interrupted)
+tty.setraw(sys.stdin.fileno())
+print("Press q to quit")
+while True:
+    DELAY = 0.5
+    #key press delay is 0.5 seconds
+    signal.setitimer(signal.ITIMER_REAL,DELAY)
+    ch = sys.stdin.read(1)
+    signal.setitimer(signal.ITIMER_REAL,0)
+    if ch == 'q':
+        termios.tcsetattr(fd,termios.TCSADRAIN, old_settings)
+        break
+    elif ch == 'w':
+        if race_mode:
+            forward(2000)
+        else:
+            forward(1600)
+    elif ch == 's':
+        backward(1400)
+    elif ch == 'a':
+        turn(1000)
+    elif ch == 'd':
+        turn(2000)
+    elif ch == 'r':
+        if race_mode:
+            print("Cruise")
+            race_mode = False
+        else:
+            print("Race mode activated")
+            race_mode = True
