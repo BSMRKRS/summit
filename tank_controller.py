3d51ae3c0d3416c0b1265e39d8ad3d3c6c82760d
chanson20 <chanson20@bsmschool.org>
2019-11-19 11:16:47 -0600

    Added tank controls UNTESTED

---
+++ tank_controller.py
@@ -0,0 +1,46 @@
+import RoboPiLib as RPL
+import sys,tty,termios,signal,setup,time
+
+Lmotor_pin = 0
+Rmotor_pin = 1
+
+
+fd = sys.stdin.fileno()
+old_settings = termios.tcgetattr(fd)
+
+def interrupted(signum, frame):
+    stop()
+
+def forward():
+    RPL.servoWrite(Lmotor_pin,2000)
+    RPL.servoWrite(Rmotor_pin,1000)
+def backward():
+    RPL.servoWrite(Rmotor_pin,2000)
+    RPL.servoWrite(Lmotor_pin,1000)
+def left():
+    RPL.servoWrite(Lmotor_pin,2000)
+    RPL.servoWrite(Rmotor_pin,2000)
+def right():
+    RPL.servoWrite(Lmotor_pin,1000)
+    RPL.servoWrite(Rmotor_pin,1000)
+def stop():
+    RPL.servoWrite(Lmotor_pin,0)
+    RPL.servoWrite(Rmotor_pin,0)
+
+
+signal.signal(signal.SIGALRM, interrupted)
+tty.setraw(sys.stdin.fileno())
+print("Press q to quit")
+while True:
+    DELAY = 0.5
+    #key press delay is 0.5 seconds
+    signal.setitimer(signal.ITIMER_REAL,DELAY)
+    ch = sys.stdin.read(1)
+    signal.setitimer(signal.ITIMER_REAL,0)
+    if ch == 'q':
+        termios.tcsetattr(fd,termios.TCSADRAIN, old_settings)
+        break
+    elif ch == 'w':
+    elif ch == 's':
+    elif ch == 'a':
+    elif ch == 'd':
